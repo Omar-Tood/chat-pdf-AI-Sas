9eb3370d16e5a0fd56fb8029974c271fa0572f88
unknown <omarjibrilabdulkhadir@gmail.com>
2024-08-01 15:25:45 +0300

    updated features landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body >{children}</body>
+      <body className="" >{children}</body>
     </html>
     </ClerkProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,44 @@ import{
 const features = [
    {
       name: "Store your PDF Documents",
-      description: "Keep all your important PDF files securely stored and easily accessible anytime, anywhere"
-   }
+      description: "Keep all your important PDF files securely stored and easily accessible anytime, anywhere",
+      icon: GlobeIcon
+   },
+   {
+      name: "Blazing Fast response",
+      description: "Experience lighting-fast answers to your, queries, ensuring you get the information you need instantly",
+      icon: ZapIcon
+   },
+   {
+      name: "Chat Memorization",
+      description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience",
+      icon: BrainCogIcon
+   },
+   {
+      name: "Interactive PDF Viewer",
+      description: "Engage with your PDFs like never before using our intuitive and interactive viewer",
+      icon: EyeIcon
+   },
+   {
+      name: "Cloud Backup",
+      description: "Rest assured knowing your documents are safely backed up on the cloud, protected from loss or damage",
+      icon: ServerCogIcon
+   },
+   {
+      name: "Responsive Across Devices",
+      description: "Access and chat with your PDFs seamlessly on any device, wether it's your desktop, tablet, or smartphone",
+      icon: MonitorCogIcon
+   },
 ]
 
 export default function Home() {
   return (
-     <div>
-        <h1>Welcome</h1>
-     </div>
+     <main className="bg-gradient-to-bl from-white to-indigo-600">
+       <div>
+         <div>
+           hello
+         </div>
+       </div>
+     </main>
   );
 }
