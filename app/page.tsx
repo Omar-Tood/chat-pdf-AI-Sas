e2aea5ff5170df02fd20e250c49f1722cc607a7b
unknown <omarjibrilabdulkhadir@gmail.com>
2024-08-01 15:05:11 +0300

    feat: implement landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
 import Image from "next/image";
+import{
+ BrainCogIcon,
+ EyeIcon,
+ GlobeIcon,
+ MonitorCogIcon,
+ ServerCogIcon,
+ ZapIcon 
+}from "lucide-react"
+
+const features = [
+   {
+      name: "Store your PDF Documents",
+      description: "Keep all your important PDF files securely stored and easily accessible anytime, anywhere"
+   }
+]
 
 export default function Home() {
   return (
